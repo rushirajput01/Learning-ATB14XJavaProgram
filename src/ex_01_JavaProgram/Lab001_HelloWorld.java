0152c7fc0ce087a203978383178168c3e96735f0
rushirajput01 <rushirajput01@gmail.com>
2025-11-04 14:09:01 +0530

    Hello created new1

--- src/ex_01_JavaProgram/Lab001_HelloWorld.java
+++ src/ex_01_JavaProgram/Lab001_HelloWorld.java
@@ -3,5 +3,6 @@ package ex_01_JavaProgram;
 public class Lab001_HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello World");
+        System.out.println("Good morning");
     }
 }
